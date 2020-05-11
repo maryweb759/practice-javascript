9bc4c4bfb7a8070ddfb59e34905af7113096432f
maryweb759 <59733319+maryweb759@users.noreply.github.com>
2020-05-11 10:31:26 +0200

    first commit

---
+++ gallery/main.js
@@ -0,0 +1,10 @@
+// we should assign the prompt to a variable
+var x = prompt("write your name", "name"); 
+if (x === "mary") 
+// location means when u click on something it opens anOther page 
+window.location = "index.html";  
+
+// change the background color 3 times
+setTimeout("document.bgColor = 'blue'", 100);
+setTimeout("document.bgColor = '#080'", 2000);
+setTimeout("document.bgColor = '#f50'", 3000);
